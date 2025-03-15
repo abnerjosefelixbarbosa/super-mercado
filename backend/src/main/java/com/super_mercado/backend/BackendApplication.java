7a079b1ff5ff2669b5a9f3fa7ede3bcaff44a059
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-03-14 22:09:47 -0300

    Update project.

--- backend/src/main/java/com/super_mercado/backend/BackendApplication.java
+++ backend/src/main/java/com/super_mercado/backend/BackendApplication.java
@@ -3,11 +3,13 @@ package com.super_mercado.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL") })
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
